777ce9c2e17df947d9613eafb247ad27136c7ed0
Paul Nzioki <palpaul49@gmail.com>
2023-09-18 15:58:51 +0300

    Update files

--- main.h
+++ main.h
@@ -2,6 +2,9 @@
 #define PRINTF_H
 
 int _printf(const char *format, ...);
-
+int _putchar(char);
+int print_str(char *);
+int print_int(int);
+int print_unsigned(unsigned int);
 
 #endif /* end PRINTF_H */
--- printf.c
+++ printf.c
@@ -2,11 +2,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
-
-int _putchar(char);
-int print_str(char *);
-int print_int(int);
-
+#include "main.h"
 /**
  * _printf - prints characters
  * @format: format to use to print characters
@@ -18,6 +14,7 @@ int _printf(const char *format, ...)
 	va_list vl;
 	int prnt, ret = 0;
 	char ch, *str;
+	unsigned int ui;
 
 	va_start(vl, format);
 
@@ -52,62 +49,14 @@ int _printf(const char *format, ...)
 					prnt = va_arg(vl, int);
 					ret += print_int(prnt);
 					break;
+				case 'u':
+					ui = va_arg(vl, unsigned int);
+					ret += print_unsigned(ui);
+					break;
 			}
 			format++;
 		}
 	}
 	va_end(vl);
 	return (ret);
-}
-
-/**
- * _putchar - print a character
- * @ch: the character to print
- *
- * Return: 1 for every character printed
- */
-int _putchar(char ch)
-{
-	return(write(1, &ch, 1));
-}
-
-/**
- * print_str - prints a string of characters
- * @str: the string to print
- *
- * Return: count of characters printed from string
- */
-int print_str(char *str)
-{
-	int i, ret = 0;
-	
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		ret += _putchar(str[i]);
-	}
-	return (ret);
-}
-
-/**
- * print_int - prints integers (uses recursion)
- * @n: the integer to print
- *
- * Return: count of printed integers
- */
-int print_int(int n)
-{
-	int i = 0;
-
-	if (n < 0)
-	{
-		i += _putchar('-');
-		n = n * -1;
-	}
-
-	if (n / 10)
-		i += print_int(n /10);
-
-	i += _putchar(n % 10 + '0');
-
-	return (i);
 }
