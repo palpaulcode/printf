5e8b5ee06fa0a83b1368e7c1c98e2b18e1298708
Paul Nzioki <palpaul49@gmail.com>
2023-09-20 02:51:29 +0300

    Update

--- cases.c
+++ cases.c
@@ -11,6 +11,9 @@ int get_case(va_list vl, const char *format)
 {
 	int ret = 0; /* ret keeps track of printed characters */
 
+	if (format == NULL)
+		return (-1);
+
 	switch (*format)
 	{
 		case 'c':
