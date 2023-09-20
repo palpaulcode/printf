278cc2c4d666f64886b801577f644ab46fa1ac4a
Paul Nzioki <palpaul49@gmail.com>
2023-09-20 03:46:54 +0300

    Update edge cases

--- printf.c
+++ printf.c
@@ -17,9 +17,9 @@ int _printf(const char *format, ...)
 	va_start(vl, format);
 
 	/* testing edge cases */
-	if (format == NULL || (format[0] == '%' && !format[1]))
+	if (format == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 
 	while (*format != '\0')
